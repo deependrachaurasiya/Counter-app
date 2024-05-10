b112994265ce5b6c71a3bbadbff959f51c2c7e22
Deependra chaurasiya <104851709+deependrachaurasiya@users.noreply.github.com>
2024-05-11 00:35:14 +0530

    Add files via upload

---
+++ counter app/counter.js
@@ -0,0 +1,11 @@
+const countValue=document.querySelector("#counter");
+function increment(){
+    let value=parseInt(countValue.innerText);
+    value=value+1;
+    countValue.innerText=value;   
+};
+function decrement(){
+    let value=parseInt(countValue.innerText);
+    value=value-1;
+    countValue.innerText=value;
+}
